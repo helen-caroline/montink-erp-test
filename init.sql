9ef8355bcb355c4fccc79c6c3a6bec2fa78cede1
Helen Caroline da Silva Santos <helencaroline@MacBook-Pro-de-Helen.local>
2025-06-22 00:47:07 -0300

    criando um volume no docker para o mysql / update na config

--- init.sql
+++ init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255),
-    preco DECIMAL(10,2)
+    preco DECIMAL(10,2),
+    estoque INT
 );
 
 CREATE TABLE variacoes (
